ee5423cb6c37ee2fd1a9406b0a449181d93a85d4
c0a24029 <c0a2402961@edu.teu.ac.jp>
2025-11-25 15:37:41 +0900

    練習問題5ゲームオーバー画面

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -2,6 +2,7 @@ import os
 import sys
 import pygame as pg
 import random
+import time
 
 WIDTH, HEIGHT = 1100, 650
 DELTA = {
@@ -26,6 +27,34 @@ def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
         tate = False
     return yoko, tate
 
+def game_over(screen: pg.Surface):
+
+    overlay = pg.Surface((WIDTH, HEIGHT))
+    overlay.fill((0, 0, 0))
+    overlay.set_alpha(200)
+
+    font = pg.font.Font(None, 100)
+    text = font.render("Game Over", True, (255, 255, 255))
+    text_rect = text.get_rect(center=(WIDTH/2, HEIGHT/2))
+
+    crying_kk_img = pg.transform.rotozoom(pg.image.load("fig/8.png"), 0, 2.0)
+
+    left_kk_rect = crying_kk_img.get_rect()
+    left_kk_rect.right = text_rect.left - 20
+    left_kk_rect.centery = text_rect.centery + 10
+    right_kk_rect = crying_kk_img.get_rect()
+    right_kk_rect.left = text_rect.right + 20
+    right_kk_rect.centery = text_rect.centery + 10
+
+    screen.blit(overlay, [0, 0])
+    screen.blit(text, text_rect)
+    screen.blit(crying_kk_img, left_kk_rect)
+    screen.blit(crying_kk_img, right_kk_rect)
+
+    pg.display.update()
+
+    time.sleep(5)
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -49,7 +78,7 @@ def main():
                 return
             
         if kk_rct.colliderect(bb_rct):
-            print("ゲームオーバー")
+            game_over(screen)
             return
         
         screen.blit(bg_img, [0, 0]) 
